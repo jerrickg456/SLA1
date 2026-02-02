a44aea8b9c90e6c74db55488701198c5c67ebe1a
Jerrick Austin <jerrickaustinm@gmail.com>
2026-02-02 22:33:02 +0530

    commit

---
+++ JS/1.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+const a=60;
+let b=9;
+b=8;
+function add(){
+    console.log(a+b);
+}
+add();
+
+integer=10;
+console.log(integer);
+
+let d=4;
+let e='SLA';
+console.log(d+e);
---
+++ JS/demo.js
@@ -0,0 +1,16 @@
+        document.getElementById("head1").innerHTML = "hello sla";
+        document.write("hello sla ");
+        document.write("welcome to sla ");
+        but.onclick = function buttonclick() {
+            document.write("button clicked");
+        };
+
+        but2.onclick=()=>{
+               document.write("button clicked using arrow function");
+        }
+        function para(){
+        console.log("hello sla");
+        }
+        para();
+
+        
