fad7bdad6e9c9e2777e58579ea822595a3a18c24
Jerrick Austin <jerrickaustinm@gmail.com>
2026-02-02 22:43:09 +0530

    commit

---
+++ .vscode/JS TASK1/1.js
@@ -0,0 +1,9 @@
+
+
+const number = 10;
+function showValue() {
+  let  value = 20;
+  console.log( value);
+}
+showValue();
+console.log("Const variable value:", number);
---
+++ .vscode/JS TASK1/3.js
@@ -0,0 +1,8 @@
+const age = 18;
+function changeAge() {
+  age = 25; 
+  console.log(age);
+}
+changeAge();
+age = 30;
+console.log(age);
