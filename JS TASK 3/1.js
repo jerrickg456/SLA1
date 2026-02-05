9b2829b1e2a12882deea241ac2d9ca1eec3d1091
Jerrick Austin <jerrickaustinm@gmail.com>
2026-02-05 22:05:13 +0530

    commit

---
+++ JS TASK 3/1.js
@@ -0,0 +1,12 @@
+
+let name = "Jerrick";       
+let age = 19;            
+let isStudent = true; 
+let result = null;     
+let marks;               
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+console.log(result);
+console.log(marks);
